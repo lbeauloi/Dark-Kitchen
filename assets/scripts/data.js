4b80f2a91aa7f236fbb2616795196e4f8a9dc02f
Pierre Marien <pierremarien29@gmail.com>
2023-09-25 15:07:21 +0200

    donnee des cartes de plats

---
+++ assets/scripts/data.js
@@ -0,0 +1,128 @@
+const starters = [
+    {
+        name: "Feuilletés jambon et mozzarella",
+        prix: 8,
+        affiche: "assets/img/feuillete_mozza_jb.webp",
+    },
+    {
+        name: "Toasts au chèvre, miel, poires et noix",
+        prix: 10,
+        affiche: "assets/img/toast_chevre_miel.webp",
+    },
+    {
+        name: "Gyozas aux shiitakés",
+        prix: 12,
+        affiche: "assets/img/gyoza.jpg",
+    },
+    { 
+        name: "Samossa d'agneau et chutney",
+        prix: 13,
+        affiche: "assets/img/Samossa.jpg",
+    },
+    {
+        name: "Caviar d'aubergine",
+        prix: 8,
+        affiche: "assets/img/caviar_aubergine.jpg",
+    },
+    {
+        name: "Falafels",
+        prix: 10,
+        affiche: "assets/img/falafels.jpg",
+    },    
+    {
+        name: "Tuna Melt",
+        prix: 12,
+        affiche: "assets/img/caviar_aubergine.jpg",
+    },    
+    {
+        name: "Ceviche de thon",
+        prix: 14,
+        affiche: "assets/img/tartare_thon.jpg",
+    },
+];
+
+const plat = [
+    {
+        name: "Poulet sauce moutarde et morilles",
+        prix: 22,
+        affiche: "assets/img/poulet_moutarde.jpg",
+    },
+    {
+        name: "Porc sauce satay",
+        prix: 19,
+        affiche: "assets/img/porc_cacahuete.jpg",
+    },
+    {
+        name: "Pasta Alfredo",
+        prix: 18,
+        affiche: "assets/img/pasta_alfredo.jpg",
+    },
+    {
+        name: "Moules au curry",
+        prix: 24,
+        affiche: "assets/img/moules_curry.webp",
+    },
+    {
+        name: "Lasagne printanière poivron et poulet",
+        prix: 20,
+        affiche: "assets/img/lasagne_printanière.jpg",
+    },
+    {
+        name: "Curry de cabillaud et ananas",
+        prix: 18,
+        affiche: "assets/img/curry_cabillaud.jpg",
+    },
+    {
+        name: "Risotto automnal courgette et champignons",
+        prix: 21,
+        affiche: "assets/img/risotto_courgette.jpg",
+    },
+    {
+        name: "Fish &amp; Chips",
+        prix: 18,
+        affiche: "assets/img/fish_chips.webp",
+    },
+]
+
+const desserts = [
+    {
+        name: "Brownie chocolat &amp; café",
+        prix: 12,
+        affiche: "assets/img/brownie.jpg",
+    },
+    {
+        name: "Gâteau aux mirabelles",
+        prix: 13,
+        affiche: "assets/img/gateau_mirabelles.webp",
+    },
+    {
+        name: "Cake aux noix",
+        prix: 10,
+        affiche: "assets/img/gateau_noix.jpg",
+    },
+    {
+        name: "Pêches roties",
+        prix: 14,
+        affiche: "assets/img/peches_roties.webp",
+    },
+    {
+        name: "Tiramisu aux myrtilles",
+        prix: 14,
+        affiche: "assets/img/tiramisu_myrtilles.webp",
+    },
+    {
+        name: "Tiramisu a la pistache",
+        prix: 15,
+        affiche: "assets/img/tiramisu_pistache.jpg",
+    },
+    {
+        name: "Cheesecake citron vert",
+        prix: 13,
+        affiche: "assets/img/cheesecake_citron.jpg",
+    },
+    {
+        name: "Corne de gazelle",
+        prix: 12,
+        affiche: "assets/img/corne_gazelle.jpg",
+    },
+]
