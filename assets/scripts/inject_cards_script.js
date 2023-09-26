00d64c3a03b91a69a6174afa2205db4f171f469d
Pierre Marien <pierremarien29@gmail.com>
2023-09-26 11:20:33 +0200

    fin du sorting et ajustement de la generatrice de carte

--- assets/scripts/inject_cards_script.js
+++ assets/scripts/inject_cards_script.js
@@ -2,44 +2,31 @@ let starter = document.querySelector(".menu__starter");
 let main = document.querySelector(".menu__mainDish");
 let sugar = document.querySelector(".menu__dessert");
 
-entree.forEach((dish) => {
-let article = document.createElement("article");
-article.innerHTML = `<figure class="card__figure">
-<img src="${dish.affiche}" >
-</figure>
-<h3>${dish.name}</h3>
-<div class="card__footer">
-<p>${dish.prix}€</p>
-<a href="#"><img src="assets/img/addToBag.png" alt="logo plus"></a>
-</div>`;
-starter.appendChild(article);
-article.setAttribute("class", "card");
-});
+function generateMenuItem(category, menuData) {
+  const menuSection = document.querySelector(`.menu__${category}`);
+  
+  menuData.forEach((dish) => {
+    const article = document.createElement("article");
+    article.innerHTML = `
+      <figure class="card__figure">
+        <img src="${dish.affiche}" alt="${dish.name}">
+      </figure>
+      <h3>${dish.name}</h3>
+      <div class="card__footer">
+        <p>${dish.prix}€</p>
+        <a href="#"><img src="assets/img/addToBag.png" alt="Add to Bag"></a>
+      </div>
+    `;
 
-plat.forEach((dish) => {
-    let article = document.createElement("article");
-    article.innerHTML = `<figure class="card__figure">
-    <img src="${dish.affiche}" >
-    </figure>
-    <h3>${dish.name}</h3>
-    <div class="card__footer">
-    <p>${dish.prix}€</p>
-    <a href="#"><img src="assets/img/addToBag.png" alt="logo plus"></a>
-    </div>`;
-    main.appendChild(article);
-    article.setAttribute("class", "card");
-    });
+    article.classList.add("card");
+    menuSection.appendChild(article);
+  });
+}
 
-desserts.forEach((dish) => {
-    let article = document.createElement("article");
-    article.innerHTML = `<figure class="card__figure">
-    <img src="${dish.affiche}" >
-    </figure>
-    <h3>${dish.name}</h3>
-    <div class="card__footer">
-    <p>${dish.prix}€</p>
-    <a href="#"><img src="assets/img/addToBag.png" alt="logo plus"></a>
-    </div>`;
-    sugar.appendChild(article);
-    article.setAttribute("class", "card");
-    });
+function generateAll() {
+  generateMenuItem("starter", entree);
+  generateMenuItem("mainDish", plat);
+  generateMenuItem("dessert", desserts);
+}
+
+generateAll();
