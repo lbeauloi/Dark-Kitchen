a95376410b2c7f5a403a5814320d4b61fa4c29e9
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-26 15:17:59 +0200

    fin fin darkmode

--- assets/scripts/darkmode.js
+++ assets/scripts/darkmode.js
@@ -1,11 +1,6 @@
 function darkMode() {
     const element = document.body;
-    let cards = document.querySelectorAll(".card");
-    element.classList.toggle("dark-mode");
-    
-    for (let card of cards){
-            card.style.backgroundColor = "#313446";
-        }
+    element.classList.toggle("dark-mode");  
  }
 
 
