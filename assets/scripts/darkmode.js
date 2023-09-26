4e1b0abe9c01d454d35f868696ffc393a4a86119
Lucas Beauloi <l.beauloi@gmail.com>
2023-09-26 11:37:05 +0200

    début darkmode

---
+++ assets/scripts/darkmode.js
@@ -0,0 +1,9 @@
+document.querySelectorAll("li").forEach((item) => {
+  item.addEventListener("click", darkMode);
+});
+
+function darkMode() {
+  let body = document.querySelector("body");
+  let mode = this.dataset.mode;
+  body.dataset.theme = mode;
+}
