eb9d302be1009e2d7a4abd3e2b8f8ec89ea8d101
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-26 15:19:15 +0200

    checkbox+ script

--- assets/scripts/darkmode.js
+++ assets/scripts/darkmode.js
@@ -9,3 +9,16 @@ function darkMode() {
  }
 
 
+let checkbox = document.querySelector("input[id=toggleSwitch]");
+const body = document.body;
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+
+
+    body.classList.add("dark-mode");
+    console.log("Checkbox is checked..");
+  } else {
+    body.classList.remove("dark-mode");
+    console.log("Checkbox is not checked..");
+  }
+});
