88567c9b2d8b01566b50271db5101fe1d38dae73
LaylaR <layla.richard.2111@gmail.com>
2023-09-27 11:44:32 +0200

    refactor basket features

---
+++ assets/scripts/BasketManager/basket_listeners.js
@@ -0,0 +1,52 @@
+﻿import{GetBasketItemFromCard, IncrementBasketItemCount, DecrementBasketItemCount, AddItemToBasket, } from "./items_manager.js";
+import {DisplayItemCount} from "./total_counts.js";
+//Get all "add" button from card elements
+let addButtons = document.querySelectorAll(".addToBasket");
+
+addItemListeners(addButtons);
+
+function addItemListeners(addButtons){
+    for (let btn of addButtons) {
+        btn.addEventListener('click', function () {
+
+            let currentCard = btn.closest(".card");
+
+            //check if item already exists in basket 
+            let basketItem = GetBasketItemFromCard(currentCard);
+
+            if (basketItem !== undefined) {
+                //if it does, increment the count for this item
+                IncrementBasketItemCount(basketItem);
+            } else {
+                //If not - create item
+                AddItemToBasket(currentCard.querySelector(".itemName").innerText, currentCard.querySelector(".itemPrice").innerText);
+            }
+
+            //update total item count;
+            DisplayItemCount();
+
+        })
+    }
+}
+
+//To call when a new item is added in the basket
+function SetIncrementInBasketListener(basketItem) {
+    basketItem.querySelector(".addItem").addEventListener('click', function () {
+
+        IncrementBasketItemCount(basketItem);
+        //update total item count;
+        DisplayItemCount();
+    });
+}
+
+function SetDecrementInBasketListener(basketItem) {
+
+    basketItem.querySelector(".removeItem").addEventListener('click', function () {
+
+        DecrementBasketItemCount(basketItem);
+        //update total item count;
+        DisplayItemCount();
+    });
+}
+
+export{SetIncrementInBasketListener, SetDecrementInBasketListener}
---
+++ assets/scripts/BasketManager/items_manager.js
@@ -0,0 +1,56 @@
+﻿import{SetDecrementInBasketListener, SetIncrementInBasketListener} from "./basket_listeners.js";
+
+function AddItemToBasket(itemName, itemPrice) {
+
+    let basketContent = document.querySelector(".basketContent");
+
+    //create item
+    let basketItem = document.createElement('div');
+    basketItem.classList.add("basketItem");
+    basketItem.innerHTML = `
+              <p class="itemName"> ${itemName.split(" ")[0]}</p>
+              <p class="unitaryPrice">${itemPrice}</p>
+              <button class="removeItem">-</button>
+              <p class="itemCount">1</p>
+              <button class="addItem">+</button>`
+
+    //Add button listeners for add and remove button directly in the basket
+    SetIncrementInBasketListener(basketItem);
+    SetDecrementInBasketListener(basketItem);
+
+    //Add item to basket
+    basketContent.appendChild(basketItem);
+}
+
+
+
+function IncrementBasketItemCount(basketItem) {
+
+    basketItem.querySelector(".itemCount").innerText++;
+
+
+}
+function DecrementBasketItemCount(basketItem) {
+    let currentCount = basketItem.querySelector(".itemCount").innerText;
+
+    if (Number(currentCount) > 1) {
+        basketItem.querySelector(".itemCount").innerText--;
+    } else if (Number(currentCount) === 1) {
+
+        //remove item from basket when count == 0;
+        basketItem.remove();
+    }
+}
+
+function GetBasketItemFromCard(card) {
+    let basketItems = document.querySelectorAll(".basketContent>.basketItem");
+
+    return Array.from(basketItems)
+        .find(i => i.querySelector(".itemName").innerText === card.querySelector(".itemName").innerText.split(' ')[0]);
+}
+
+
+export{AddItemToBasket, IncrementBasketItemCount, DecrementBasketItemCount, GetBasketItemFromCard}
+
+
+
